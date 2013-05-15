75e1c41b13b354eb0ff506db103dbf3e5fa2c8a4
Hunter Owens <owens.hunter@gmail.com>
2013-05-14 21:19:17 -0500

    models broken, some controllers

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import os
 from flask import Flask, render_template, request
 import stripe
 from auth import requires_auth
-
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import *
+from models import User, Operator, Item, Order
 stripe_keys = {
     'secret_key': os.environ['SECRET_KEY'],
     'publishable_key': os.environ['PUBLISHABLE_KEY']
@@ -10,13 +13,18 @@ stripe_keys = {
 
 stripe.api_key = stripe_keys['secret_key']
 
+engine = create_engine("mysql://root:@localhost/Truckate")
+
+SessionMkr = sessionmaker()
+SessionMkr.configure(bind=engine)
+session = SessionMkr()
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', key=stripe_keys['publishable_key'])
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html', key=stripe_keys['publishable_key'])
 
-@requires_auth
 @app.route('/charge', methods=['POST'])
 def charge():
     # Amount in cents
@@ -35,6 +43,15 @@ def charge():
     )
 
     return render_template('charge.html', amount=amount)
+
+@app.route('/index')
+def index():
+    operators = [instance.location for instance in session.query(Operator)]
+    print operators
+    #bathrooms = session.query(Bathroom).all()
+    #print bathrooms
+    #return render_template('list.html', locs=locations, baths=bathrooms)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
