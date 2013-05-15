5914813b36e73cdb2ffd5f96782570c4b9581a13
Hunter Owens <owens.hunter@gmail.com>
2013-05-14 20:33:48 -0500

    models done

--- models.py
+++ models.py
@@ -11,33 +11,56 @@ session = Session()
 
 Base = declarative_base()
 
+
+association_table = Table('Orders_Items', Base.metadata,
+        Column('order_id', Integer, ForeignKey('orders.id')),
+        Column('item_id', Integer, ForeignKey('items.id'))
+)
+
 class User(Base):
         __tablename__ = 'users'
         id = Column(Integer,primary_key=True)
         email = Column(String(200))
+        firstName = Column(String(50))
+        lastName = Column(String(50))
         password = Column(String(200))
-        phone = Column(Integer)
+        phone = Column(String(20))
 
 class Operator(Base):
         __tablename__ = 'operators'
         id = Column(Integer,primary_key=True)
         email = Column(String(200))
         password = Column(String(200))
-        phone = Column(Integer)
+        phone = Column(String(20))
         open = Column(Boolean)
+        name = Column(String(200))
 
 class Item(Base):
         __tablename__ = 'items'
+        id = Column(Integer,primary_key=True)
         name = Column(String(200))
         price = Column(Float)
+        desc = Column(String(200))
+        count = Column(Integer)
         operator_id = Column(Integer, ForeignKey('operators.id'))
         operator = relationship("Operator",backref=backref('item',order_by=id))
+
+class Order(Base):
+        __tablename__ = 'orders'
+        id = Column(Integer,primary_key=True)
+        active = Column(Boolean)
+        due = Column(DateTime)
+        operator_id = Column(Integer, ForeignKey('operators.id'))
+        operator = relationship("Operator",backref=backref('item',order_by=id))
+        items = relationship("Items",
+                    secondary=association_table)
+
 
 
 
 Base.metadata.create_all(engine)
 
-print 'Created'
+#print 'Created'
 #bathroom_new = Bathroom(location="harper", floor="1", gender="male")
 #review_new = Review(content="stuff", rating=3)
 #review_new.bathroom = bathroom_new
